7c58cbd14d14ec30858bf42ff1a36606fe8a95a2
Aydrian Howard <aydrian@gmail.com>
2020-03-05 14:52:58 -0500

    handle Courier API exceptions

--- trycourier/exceptions.py
+++ trycourier/exceptions.py
@@ -1,2 +1,12 @@
 class CourierAPIException(Exception):
-    pass
+    response = None
+    message = "An unknown error occurred"
+
+    def __init__(self, response):
+        self.response = response
+        self.message = """Call to {uri} returned {status_code}
+        {msg}""".format(
+            uri=response.url,
+            status_code=response.status_code,
+            msg=response.json()
+        )
