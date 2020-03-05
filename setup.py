29ba280efdcbef0bfb12ac0cc919d3ac3dfc4469
Aydrian Howard <aydrian@gmail.com>
2020-03-05 12:00:29 -0500

    added files for PyPI distribution

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trycourier",
+    version="1.0.0",
+    author="Courier",
+    author_email="support@trycourier.com",
+    description="A Python Package for communicating with the Courier REST API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trycourier/courier-python",
+    packages=setuptools.find_packages(),
+    install_requires=['requests>=2.23.0'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
