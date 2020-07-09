ba15a94fd12a635803d2483384ac76afeaa269b0
Aydrian Howard <aydrian@gmail.com>
2020-07-09 14:00:07 -0400

    Added a configurable timeout to the CourierAPISession

--- trycourier/session.py
+++ trycourier/session.py
@@ -1,10 +1,22 @@
 from base64 import b64encode
 from requests import Session
+from requests.adapters import HTTPAdapter
+
+
+class MyHTTPAdapter(HTTPAdapter):
+
+    def __init__(self, timeout=None, *args, **kwargs):
+        self.timeout = timeout
+        super(MyHTTPAdapter, self).__init__(*args, **kwargs)
+
+    def send(self, *args, **kwargs):
+        kwargs['timeout'] = self.timeout
+        return super(MyHTTPAdapter, self).send(*args, **kwargs)
 
 
 class CourierAPISession(Session):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, timeout=5, *args, **kwargs):
         """
         Creates a new CoreAPISession instance.
         """
@@ -13,6 +25,8 @@ class CourierAPISession(Session):
         self.headers.update({
             'Content-Type': 'application/json'
         })
+
+        self.mount("https://", MyHTTPAdapter(timeout=timeout))
 
     def init_library_version(self, version):
         self.headers.update({
