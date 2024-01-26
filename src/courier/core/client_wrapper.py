99164b9cf6a731152402358f541ebeea5f09f4c5
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-26 17:27:59 +0000

    SDK regeneration

--- src/courier/core/client_wrapper.py
+++ src/courier/core/client_wrapper.py
@@ -13,8 +13,8 @@ class BaseClientWrapper:
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
-            "X-Fern-SDK-Name": "trycourier",
-            "X-Fern-SDK-Version": "6.0.0b0",
+            "X-Fern-SDK-Name": "fern-courier",
+            "X-Fern-SDK-Version": "v6.0.0b0",
         }
         headers["Authorization"] = f"Bearer {self._get_authorization_token()}"
         return headers
