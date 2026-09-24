c103ad3a81063bc0b936c8a26323919eadbe5aaf
Mike Miller <mike@mikemiller.design>
2026-09-24 12:57:58 -0700

    release: 9.11.0 (#209)

--- src/courier/_version.py
+++ src/courier/_version.py
@@ -1,4 +1,4 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 __title__ = "courier"
-__version__ = "9.9.0"  # x-release-please-version
+__version__ = "9.11.0"  # x-release-please-version
